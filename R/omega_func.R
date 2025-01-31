9ddcbd526973fd7caed6ca9fc22f5cabe2cab417
CRH1998 <brf337@alumni.ku.dk>
2025-01-31 11:42:40 +0100

    Second commit

--- R/omega_func.R
+++ R/omega_func.R
@@ -1,3 +1,16 @@
+#' Title
+#'
+#' @param semi_def_matrix A list containing the three matrices, householder matrix, kinship matrix, identity matrix.
+#' @param sigma2_vec A vector containing the variances of the random effects in the same order as the three matrices.
+#'
+#' @returns The variance/covariance matrix Sigma
+#' @export
+#'
+#' @examples
+#' mat1 <- matrix(1:9, ncol = 3)
+#' mat2 <- matrix(10:18, ncol = 3)
+#' mat3 <- matrix(19:27, ncol = 3)
+#' omega_func(semi_def_matrix = list(mat1, mat2, mat3), sigma2_vec = c(1,2,3))
 omega_func <- function(semi_def_matrix, sigma2_vec){
 
   omega <- 0
